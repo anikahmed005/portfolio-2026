057b1684db34beecdce5569ec16f3871f5d4de3c
anikahmed005 <54121455+anikahmed005@users.noreply.github.com>
2026-03-30 18:38:42 -0400

    Add Microsoft Clarity for user activity tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from '@microsoft/clarity'
 import '@ds/tokens/inject'
 import '@ds/tokens/globals.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext'
+
+Clarity.init('qpoynbbcm1')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
